bab1b879aeeeabc4c9127feb0aa078833d4b9d8d
Joseph Driedger <jdriedger3@my.bcit.ca>
2022-12-20 20:46:15 -0800

    Added a main to index

---
+++ public/external/js/script_index.js
@@ -0,0 +1,11 @@
+function about() {
+    window.location.href = "./external/html/about.html";
+}
+
+function projects() {
+    window.location.href = "./external/html/projects.html";
+}
+
+function posts() {
+    window.location.href = "./external/html/posts.html";
+}
